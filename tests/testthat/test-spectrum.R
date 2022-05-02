0c5842631aa7011e059b4423e8d7a33d8d6ebdf9
Jeff Eaton <7427899+jeffeaton@users.noreply.github.com>
2022-05-02 19:42:43 +0100

    update Spectrum tests

--- tests/testthat/test-spectrum.R
+++ tests/testthat/test-spectrum.R
@@ -13,20 +13,20 @@ test_that("DemProj only matches EPP-ASM", {
   mod <- eppasm::simmod(fp)
 
   demp <- prepare_leapfrog_demp(pjnz1)
-  lmod <- leapfrogR(demp$basepop[,,1], demp$Sx, demp$netmigr_adj,
-                    demp$asfr, demp$births_sex_prop)
+  hivp <- prepare_leapfrog_projp(pjnz1)
+  lmod <- leapfrogR(demp, hivp)
 
-  expect_equal(lmod[16:80,,], mod[1:65,,1,])
+  expect_equal(lmod$totpop1[16:80,,], mod[1:65,,1,])
 })
 
 test_that("Leapfrog matches DemProj projection without migration", {
 
   pjnz1 <- test_path("../testdata/spectrum/v6.13/bwa_demproj-only-no-mig_spectrum-v6.13_2022-02-12.PJNZ")
   demp1 <- prepare_leapfrog_demp(pjnz1)
-  lmod1 <- leapfrogR(demp1$basepop[,,1], demp1$Sx, demp1$netmigr_adj,
-                     demp1$asfr, demp1$births_sex_prop)
+  hivp1 <- prepare_leapfrog_projp(pjnz1)
+  lmod1 <- leapfrogR(demp1, hivp1)
 
-  diff <- lmod1[,,2] - demp1$basepop[,,2]
+  diff <- lmod1$totpop[,,2] - demp1$basepop[,,2]
 
   ## For age 0, there is a difference with Spectrum; unsure why
   diff[1,]
@@ -38,10 +38,10 @@ test_that("Leapfrog matches DemProj projection with migration", {
 
   pjnz1 <- test_path("../testdata/spectrum/v6.13/bwa_demproj-only_spectrum-v6.13_2022-02-12.PJNZ")
   demp1 <- prepare_leapfrog_demp(pjnz1)
-  lmod1 <- leapfrogR(demp1$basepop[,,1], demp1$Sx, demp1$netmigr_adj,
-                     demp1$asfr, demp1$births_sex_prop)
+  hivp1 <- prepare_leapfrog_projp(pjnz1)
+  lmod1 <- leapfrogR(demp1, hivp1)
 
-  diff <- lmod1[,,2] - demp1$basepop[,,2]
+  diff <- lmod1$totpop1[,,2] - demp1$basepop[,,2]
 
   ## With migration, difference goes through age 5; suspect some Beers
   ## step in Spectrum that is missing?
