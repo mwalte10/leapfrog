60c45da3863adc6db0530748efb38674ccf99e52
maggie <mwalters@imperial.ac.uk>
2022-06-27 16:03:05 +0100

    adding testing for transmission model

--- tests/testthat/test-spectrum.R
+++ tests/testthat/test-spectrum.R
@@ -61,3 +61,105 @@ test_that("Leapfrog matches DemProj projection with migration", {
 
   expect_true(all(abs(diff) < 0.01))
 })
+
+test_that("Leapfrog matches direct incidence option in EPP-ASM, no ART", {
+  ## Check that prevalence, deaths and incidence  matches between
+  ## the two models
+  pjnz1 <- test_path("../testdata/spectrum/v6.13/bwa_aim-adult-no-art_spectrum-v6.13_2022-02-12.pjnz")
+
+  demp <- prepare_leapfrog_demp(pjnz1)
+  hivp <- prepare_leapfrog_projp(pjnz1)
+
+  ## Replace netmigr with unadjusted age 0-4 netmigr, which are not
+  ## in EPP-ASM preparation
+  demp$netmigr <- read_netmigr(pjnz1, adjust_u5mig = FALSE)
+  demp$netmigr_adj <- adjust_spectrum_netmigr(demp$netmigr)
+
+  lmod <- leapfrogR(demp, hivp)
+
+  expect_warning(fp <- eppasm::prepare_directincid(pjnz1),
+                 "no non-missing arguments to min; returning Inf")
+  fp$tARTstart <- 61L
+
+  ## Replace ASFR because demp$asfr is normalised, but fp$asfr is not
+  fp$asfr <- demp$asfr
+
+  mod <- eppasm::simmod(fp)
+
+  ## PREVALENCE
+  ## mod is at 5 year age intervals so lets just collapse all to 15+
+  ## Unsure what the age groups are here actually, this is what it seems:
+  ## mod: 1-2 = lmod: 16:20 and then 5 year increments after?
+  mod_prev <- attr(mod, 'hivpop')
+  mod_prev <- melt(mod_prev)
+  mod_prev <- data.table(mod_prev)
+  mod_prev <- mod_prev[,.(value = sum(value)), by = c('Var3', 'Var4')]
+
+  lmod_prev <- lmod$hivpop1
+  lmod_prev <- melt(lmod_prev)
+  lmod_prev <- data.table(lmod_prev)
+  lmod_prev <- lmod_prev[Var1 %in% c(16:80)]
+  lmod_prev <- lmod_prev[,.(value = sum(value)), by = c('Var2', 'Var3')]
+  setnames(mod_prev, c('Var3', 'Var4', 'value'), c('sex', 'year', 'eppasm'))
+  setnames(lmod_prev, c('Var2', 'Var3', 'value'), c('sex', 'year', 'leapfrog'))
+  prev_comparison <- merge(mod_prev, lmod_prev, by = c('sex', 'year'))
+  prev_comparison[,pct_diff := abs((eppasm - leapfrog) / leapfrog)]
+  prev_comparison[eppasm == 0, pct_diff := 0]
+
+  ##right now lets set the percent difference as less than 5%
+  expect_true(all(prev_comparison$pct_diff < 0.05))
+
+  ##INCIDENCE
+  mod_inc <- attr(mod, 'infections')
+  mod_inc <- mod_inc[-66,,]
+  mod_inc <- data.table(melt(mod_inc))
+  lmod_inc <- lmod$infections
+  lmod_inc <- lmod_inc[16:80,,]
+  lmod_inc <- data.table(melt(lmod_inc))
+  inc_comparison <- merge(mod_inc, lmod_inc, all.x = T, by = c('Var1', 'Var2', 'Var3'))
+  inc_comparison[,diff := abs((value.x - value.y) / value.x)]
+  inc_comparison[value.x == 0, diff := 0]
+  ## super small differences so can't do this, will just do pct diff less than 1%
+  ##expect_equal(lmod_inc[16:80,,], mod_inc[1:65,,])
+  expect_true(all(inc_comparison$diff < 1e-2))
+
+  ##HIV DEATHS
+  mod_deaths <- attr(mod, 'hivdeaths')
+  lmod_deaths <- lmod$hivdeaths
+  mod_deaths <- mod_deaths[-66,,]
+  mod_deaths <- data.table(melt(mod_deaths))
+  lmod_deaths <- lmod$infections
+  lmod_deaths <- lmod_deaths[16:80,,]
+  lmod_deaths <- data.table(melt(lmod_deaths))
+  deaths_comparison <- merge(mod_deaths, lmod_deaths, all.x = T, by = c('Var1', 'Var2', 'Var3'))
+  deaths_comparison[,diff := abs((value.x - value.y) / value.x)]
+  deaths_comparison[value.x == 0, diff := 0]
+  ##looks like deaths are fairly different, this is what is driving difference in prevalence above
+
+
+})
+
+test_that("Dimensions for ages are all equal",{
+  pjnz1 <- test_path("../testdata/spectrum/v6.13/bwa_demproj-only_spectrum-v6.13_2022-02-12.PJNZ")
+  demp1 <- prepare_leapfrog_demp(pjnz1)
+  hivp1 <- prepare_leapfrog_projp(pjnz1)
+
+  ##full pop
+  basepop_ages <- dim(demp1$basepop)[1]
+  mx_ages <- dim(demp1$mx)[1]
+  netmigr_ages <- dim(demp1$netmigr)[1]
+  Sx_ages <- dim(demp1$Sx)[1] -1
+  netmigr_adg_ages <- dim(demp1$netmigr_adj)[1]
+
+  age_dim <- c(basepop_ages, mx_ages, netmigr_ages, Sx_ages, netmigr_adg_ages)
+  expect_equal(length(unique(age_dim)), 1)
+
+  ##adult pop
+  asfr_ages <- dim(demp1$asfr)[1]
+  asfd_ages <- dim(demp1$asfd)[1]
+
+  expect_equal(asfr_ages, asfd_ages)
+})
+
+## check a direct incidence with 0 input incidence
+## check that incidence pattern looks normal, following incrr_age
